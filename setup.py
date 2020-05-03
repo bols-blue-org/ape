0d72fc6128cc91590f14e7c4b2f41d30231a7e9c
Hisayoshi Suehiro <hisayoshi.suehiro@access-company.com>
2020-05-03 15:36:46 +0900

    ミスを修正

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
+from setuptools import find_packages, setup
+
 setup(
     name='pid_envaluation',
-    version='0.0.1',
+    version='0.0.2',
     description='Ardupilot pid envaluation tool',
-    long_description=readme,
+    long_description="",
     author='Hisayoshi Suehiro',
     author_email='bols-blue@lnc.jp',
     url='https://github.com/bols-blue-org/pid_evaluation',
