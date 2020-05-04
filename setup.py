4de56441deedfcf5617cc232e015273c803f11a8
Hisayoshi Suehiro <hisayoshi.suehiro@access-company.com>
2020-05-04 19:15:00 +0900

    セットアップスクリプトを修正

--- setup.py
+++ setup.py
@@ -9,5 +9,10 @@ setup(
     author_email='bols-blue@lnc.jp',
     url='https://github.com/bols-blue-org/pid_evaluation',
     license=license,
+    entry_points={
+        'console_scripts': [
+            'pid_envaluation = pid_envaluation.data_collection:main',
+        ],
+    },
     packages=find_packages(exclude=('tests', 'docs'))
 )
